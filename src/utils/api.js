f11cff4aebbc6c7b74af09a997132e08c5b2b131
young <21738331@qq.com>
2018-04-03 08:10:14 +0800

    modify api request

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,26 +3,26 @@ import {
 } from '../utils/wxRequest';
 import wepy from 'wepy';
 
-// const host = 'http://git.com'
-const host = 'https://xiaoyounger.com'
+const host = 'http://git.com'
+// const host = 'https://xiaoyounger.com'
 // const host = 'https://yiiiblog.com'
 
 const apiUrl = host + '/api'
 // 获取首页数据
 const getIndexList = (params) => wxRequest(params, `${apiUrl}/index`);
 const getIndexHeader = (params) => wxRequest(params, `${apiUrl}/header`);
-const getIndexBill = (params) => wxRequest(params, `${apiUrl}/bill`);
+const getIndexBill = (id) => wxRequest({}, `${apiUrl}/statement/${id}`);
 
 // 获取账单
 const Bill = (params, c_id = 0) => {
-  let url = `${apiUrl}/bills`
+  let url = `${apiUrl}/statement`
   if (c_id != null && c_id != 0) url = `${url}/${c_id}`
   return wxRequest(params, url)
 }
-const getBillFrequentUsed = (params) => wxRequest(params, `${apiUrl}/bills/frequent_used`);
-const getBillAssets = (params) => wxRequest(params, `${apiUrl}/bills/assets`);
-const getExpendCategories = (params) => wxRequest(params, `${apiUrl}/bills/expend_categories`);
-const getIncomeCategories = (params) => wxRequest(params, `${apiUrl}/bills/income_categories`);
+const getBillFrequentUsed = (params) => wxRequest(params, `${apiUrl}/statements/frequent_used`);
+const getBillAssets = (params) => wxRequest(params, `${apiUrl}/statements/assets`);
+const getExpendCategories = (params) => wxRequest(params, `${apiUrl}/statements/expend_categories`);
+const getIncomeCategories = (params) => wxRequest(params, `${apiUrl}/statements/income_categories`);
 
 // 图表栏的数据获取
 const Chart = (params) => wxRequest(params, `${apiUrl}/chart`);
@@ -52,12 +52,12 @@ const WalletParent = (params) => wxRequest(params, `${apiUrl}/wallet/parent`);
 const WalletList = (params) => wxRequest(params, `${apiUrl}/wallet/list`);
 
 const Category = (params, c_id = 0) => {
-  let url = `${apiUrl}/category`
+  let url = `${apiUrl}/categories`
   if (c_id != null && c_id != 0) url = `${url}/${c_id}`
   return wxRequest(params, url)
 }
 
-const CategoryParent = (params) => wxRequest(params, `${apiUrl}/category/parent`);
+const CategoryParent = (params) => wxRequest(params, `${apiUrl}/categories/parent`);
 const CategoryIcon = () => wxRequest({}, `${apiUrl}/icons/categories`);
 
 // 个人设置
