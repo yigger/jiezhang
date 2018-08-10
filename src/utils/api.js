7bdbff39770aea949e28fe8ecb7523fdac2d4bb5
young <21738331@qq.com>
2018-08-10 08:45:49 +0800

    refactor: refactor index page. only show week data

--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-export default {
-  IndexList: 'index', // 首页数据
-  IndexHeader: 'header', // 首页头部数据
-  Chart: 'chart', // 统计页面
-  Wallet: 'wallet', // 资产页面
-  Setting: 'settings', // 设置
-  SuperChart: 'super_chart', // 图表分析
-  UpdateUser: 'users/update_user', // 更新用户信息
-}
